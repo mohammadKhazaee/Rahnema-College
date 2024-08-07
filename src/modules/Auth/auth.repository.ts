ddb6b84c44ff1b7410aebff83800a939868451d0
ehsAnhAq86 <ehsunhagh86@gmail.com>
2024-08-07 14:16:36 +0330

    feat: creat-dto-and-resetpassword-function

--- src/modules/Auth/auth.repository.ts
+++ src/modules/Auth/auth.repository.ts
@@ -1,9 +1,38 @@
 import { Repository, DataSource } from 'typeorm';
+import { User } from '../User/model/user';
+import { sign } from 'jsonwebtoken';
+import Jwt from 'jsonwebtoken';
+import { ResetPaswordDto } from './dto/resetpassword-dto';
+const { OAuth2 } = require('googleapis').auth;
+import nodemailer from "nodemailer";
+import { HttpError } from '../../utility/errors';
+import { z } from 'zod';
+
+const oauth2Client = new OAuth2(
+    'YOUR_CLIENT_ID',
+    'YOUR_CLIENT_SECRET',
+    'https://developers.google.com/oauthplayground'
+);
+oauth2Client.setCredentials({ refresh_token: 'YOUR_REFRESH_TOKEN' });
+
 
 export class AuthRepository {
     private userRepo: Repository<any>;
 
     constructor(dataSource: DataSource) {
         this.userRepo = dataSource.getRepository({} as any);
+    }
+
+    public generateTokenForLogin(user: User, rememberMe?: boolean) {
+        const expiresIn = rememberMe ? '30d' : '1w';
+        const secret = 'Rebuilders';
+        const payload = { username: user.username, email: user.email };
+        return sign(payload, secret, { expiresIn });
+    }
+
+
+    public generateTokenForReset(user: User) {
+        const secretKey = 'randomsecretkeydorresetpass1593574862'
+        return Jwt.sign({ email: user.email }, secretKey, { expiresIn: '24h' })
     }
 }
--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,23 +1,25 @@
-import errorMap from 'zod/lib/locales/en';
-import { AppDataSource } from '../../data-source';
 import { User } from '../User/model/user';
 import { UserRepository } from '../User/user.repository';
-import { UserService } from '../User/user.service';
 import { AuthRepository } from './auth.repository';
 import { LoginDto } from './dto/logindto';
-import { sign } from 'jsonwebtoken';
+import { HttpError, NotFoundError } from '../../utility/errors';
+import { ResetPaswordDto } from './dto/resetpassword-dto';
+const { OAuth2 } = require('googleapis').auth;
+import nodemailer from "nodemailer";
 
+const oauth2Client = new OAuth2(
+    '288101966109 - dd3lfoi3t08m8e0pkvr0fthn55gasfr3.apps.googleusercontent.com',
+    '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',
+    'https://developers.google.com/oauthplayground'
+);
+
+oauth2Client.setCredentials({ refresh_token: '1//04VaiSbR1m0xzCgYIARAAGAQSNwF-L9Irvzi59ha1N3VYcFsXqWj0GfTbz9Awt5eNecgYtgpkVRiAOc4zKbwJB3xE_zQZV7id2wI' });
+
+
+/// ============= classes
 export class AuthService {
     constructor(
-        private authRepo: AuthRepository,
-        private userService: UserService
-    ) { }
-}
-export class userService {
-    private userRepo: UserRepository;
-    constructor() {
-        this.userRepo = new UserRepository(AppDataSource);
-    }
+        private authRepo: AuthRepository, private userRepo: UserRepository) { }
 
     async login(dto: LoginDto) {
         let user: User | null;
@@ -31,41 +33,63 @@ export class userService {
         if (dto.password !== user.password) {
             throw new Error('401, passwort is wrong');
         }
-        const token = this.generateToken(user);
+        const token = this.authRepo.generateTokenForLogin(user);
         return token;
     }
-    private generateToken(user: User, rememberMe?: boolean) {
-        const expiresIn = rememberMe ? '30d' : '1w';
-        const secret = 'Rebuilders';
-        const payload = { username: user.username, email: user.email };
-        return sign(payload, secret, { expiresIn });
-    }
-}
-export class userService {
-    private userRepo: UserRepository;
-    constructor() {
-        this.userRepo = new UserRepository(AppDataSource);
-    }
 
-    async login(dto: LoginDto) {
-        let user: User | null;
-        if ('username' in dto)
-            user = await this.userRepo.findByUsername(dto.username);
-        else user = await this.userRepo.findByEmail(dto.email);
+    async resetPassword(dto: ResetPaswordDto) {
+        let user: User | null
+        if ("username" in dto)
+            user = await this.userRepo.findByUsername(dto.username)
+        else user = await this.userRepo.findByEmail(dto.email)
 
         if (!user) {
-            throw new Error('401 , username or password not found');
+            throw new NotFoundError()
         }
-        if (dto.password !== user.password) {
-            throw new Error('401, passwort is wrong');
-        }
-        const token = this.generateToken(user);
-        return token;
+
+
+        const accessToken = await oauth2Client.getAccessToken();
+        const resetToken = this.authRepo.generateTokenForReset(user)
+        const resetLink = `https://RebuildersGeram.com/reset-password?token=${resetToken}`;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: "OAuth2",
+                user: 'ehsunhagh86@gmail.com',
+                clientId: '288101966109 - dd3lfoi3t08m8e0pkvr0fthn55gasfr3.apps.googleusercontent.com',
+                clientSecret: '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',
+                refreshToken: '1//04VaiSbR1m0xzCgYIARAAGAQSNwF-L9Irvzi59ha1N3VYcFsXqWj0GfTbz9Awt5eNecgYtgpkVRiAOc4zKbwJB3xE_zQZV7id2wI',
+                accessToken: accessToken.token
+            }
+
+        })
+
+
+
+        const mailOptions = {
+            from: "ehsunhagh86@gmail.com",
+            to: user.email,
+            subject: "Reset Your Password - RebuildersGeram",
+            text: `
+        Hi,
+
+        We received a request to reset your password for your account on MyApp.
+
+        Click the link below to reset your password:
+        ${resetLink}
+
+        If you didn't request a password reset, please ignore this email. If you have any issues, contact our support team at support@myapp.com.
+
+        Best regards,
+        The MyApp Team
+
+        For more information about how we handle your data, please see our Privacy Policy at https://www.myapp.com/privacy-policy.
+      `
+        };
+        const result = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', result);
+
     }
-    private generateToken(user: User, rememberMe?: boolean) {
-        const expiresIn = rememberMe ? '30d' : '1w';
-        const secret = 'Rebuilders';
-        const payload = { username: user.username, email: user.email };
-        return sign(payload, secret, { expiresIn });
-    }
+
 }
