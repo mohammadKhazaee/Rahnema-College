7f23ca243075b25ad30bd7fd25e46e099316a669
mohammadKhazaee <m.khazaee.p@gmail.com>
2024-09-06 11:15:03 +0330

    Changed higher level services folder location

--- src/modules/Common/social.service.ts
+++ src/modules/Common/social.service.ts
@@ -1,14 +1,15 @@
-import { UserRelationService } from '../modules/UserRelation/user-relation.service';
-import { PostService } from '../modules/Post/post.service';
-import { UserProfileDao } from '../modules/User/model/user';
-import { UserService } from '../modules/User/user.service';
-import { explorePostsDto } from '../modules/Post/model/post';
+import { explorePostsDto } from '../Post/model/post';
+import { PostService } from '../Post/post.service';
+import { UserProfileDao } from '../User/model/user';
+import { UserService } from '../User/user.service';
+import { UserRelationService } from '../UserRelation/user-relation.service';
+
 export class SocialService {
     constructor(
         private userService: UserService,
         private followService: UserRelationService,
         private postService: PostService
-    ) { }
+    ) {}
 
     async getUserInfo(username: string): Promise<UserProfileDao> {
         const user = await this.userService.getUser({ username });
@@ -34,16 +35,16 @@ export class SocialService {
     }
 
     async getSocialExplore(username: string) {
-        const explore = await this.postService.exlorePosts(username)
+        const explore = await this.postService.exlorePosts(username);
         const returnExplore: explorePostsDto[] = await Promise.all(
             explore.map(async (p) => ({
                 ...p,
                 creator: {
                     ...p.creator,
-                    followersCount: await this.followService.getFollowersCount(p.creator.username)
-                }
+                    followersCount: await this.followService.getFollowersCount(p.creator.username),
+                },
             }))
-        )
-        return returnExplore
+        );
+        return returnExplore;
     }
 }
